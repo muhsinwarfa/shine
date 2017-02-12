b8d886dbeb42d1bf34fa791bf882a33d4bd62aa7
Muhsin <muhsin.warfa@gmail.com>
2017-02-12 22:47:40 +0000

    added customers controller plus put content in customers index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+
   devise_for :users
     root 'dashboard#index'
+    resources :customers, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
