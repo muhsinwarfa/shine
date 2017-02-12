dbd289010bf717e2456bd18d65f562e63281b1ed
Muhsin <muhsin.warfa@gmail.com>
2017-02-12 20:12:53 +0000

    new migration for checking email constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128215153) do
+ActiveRecord::Schema.define(version: 20170212192328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
